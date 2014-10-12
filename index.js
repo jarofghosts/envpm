7d3be38f13dbd6edf26d7d1f45c190358419b905
jesse keane <me@jessekeane.me>
2014-10-12 14:29:32 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 var spawn = require('child_process').spawn
-  , fs = require('fs')
 
-var find_file = require('fs-find-root').file
+var findFile = require('fs-find-root').file
   , which = require('which')
 
 module.exports = envpm
 
 function envpm(dir, args, _exec) {
-  var exec_npm = _exec || exec
+  var execNpm = _exec || exec
 
-  find_file('.npmrc', dir, run_npm)
+  findFile('.npmrc', dir, run_npm)
 
   function run_npm(err, found) {
-    if(err || !found) return exec_npm(args)
+    if(err || !found) return execNpm(args)
 
-    exec_npm(args.concat(['--userconfig', found]))
+    execNpm(args.concat(['--userconfig', found]))
   }
 }
 
 function exec(args) {
-  var run_options = {stdio: 'inherit'} 
-    , node = which.sync('node')
+  var node = which.sync('node')
     , npm = which.sync('npm')
 
   spawn(node, [npm].concat(args), {stdio: 'inherit'})
